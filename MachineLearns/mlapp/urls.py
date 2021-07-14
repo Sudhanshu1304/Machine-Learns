a606333927d08f8f480bd72b52adcc476cde41f0
SSudhanshu <sudhanshu.dpandey@gmail.com>
2021-07-15 05:19:58 +0530

    Added noice page

--- MachineLearns/mlapp/urls.py
+++ MachineLearns/mlapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("",views.home,name='home'),
     path("encoder/",views.encoder,name='Encoder'),
     path("encoder/encosize/",views.encoder_size,name='Encoder Size'),
+    path("encoder/enconoice/",views.encoder_noice,name='Encoder Noice'),
 ]
